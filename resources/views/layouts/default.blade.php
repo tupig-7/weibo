614eb2c9e9850f3ee9159c76877bc13032e50a72
Everythone <2664006323@qq.com>
2019-04-15 16:38:03 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,8 +3,21 @@
 <html>
 <head>
   <title>@yield('title', 'Weibo App') - 新手教程</title>
+  <link rel="stylesheet" type="text/css" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="/">Weibo App</a>
+      <ul class="navbar-nav justify-content-end">
+        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">登陆</a></li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container">
+    @yield('content')
+  </div>
+
 </body>
 </html>
